913f8fc482e1a76b3df6d83b5663ec42c4056799
Chirag Arora <chiragarora44.ca@gmail.com>
2024-06-10 18:49:52 +0530

    admin menu

--- src/app/menu-items/page.jsx
+++ src/app/menu-items/page.jsx
@@ -3,10 +3,21 @@
 import { useProfile } from '@/components/UseProfile'
 import UserTabs from '@/components/layout/UserTabs'
 import Link from 'next/link'
-
+import {useState, useEffect} from "react";
+import Image from "next/image"
 
 const page = () => {
+    const [menuItems, setMenuItems] =  useState([])
     const {loading, data} = useProfile()
+
+    useEffect(() => {
+      fetch('/api/menu-items').then(res => {
+        res.json().then(menuItems => {
+            setMenuItems(menuItems)
+        })
+      })
+    }, [])
+    
 
     if(loading){
         return 'loading user info...'
@@ -24,27 +35,18 @@ const page = () => {
                 <span>Crete new menu item</span>
             </Link>
         </div>
-        {/* <div>
-            <h2 className="text-sm text-gray-500 mt-8">Edit menu item:</h2>
-            <div className="grid grid-cols-3 gap-2">
-            {menuItems?.length > 0 && menuItems.map(item => (
-                <Link
-                key={item._id}
-                href={'/menu-items/edit/'+item._id}
-                className="bg-gray-200 rounded-lg p-4"
-                >
-                <div className="relative">
-                    <Image
-                    className="rounded-md"
-                    src={item.image} alt={''} width={200} height={200} />
-                </div>
-                <div className="text-center">
+        <div>
+            <h2 className='text-sm text-gray-500 mt-8'>EDIT MENU ITEMS</h2>
+            <div className='grid grid-cols-4'></div>
+            {menuItems?.length>0 && menuItems.map(item => (
+                <Link href={'/menu-items/edit/'+item._id} className='button mb-1 bg-white'>
+                    <div className='relative w-24 h-24'>
+                        <Image src={item.image} alt={""} width={100} height={100} />
+                    </div>
                     {item.name}
-                </div>
                 </Link>
             ))}
-            </div>
-        </div> */}
+        </div>
     </section>
   )
 }
